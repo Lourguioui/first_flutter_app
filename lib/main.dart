4f481a266067871a240541ac3bce663934066b01
Lourguioui <iz_lourguioui@esi.dz>
2020-01-03 04:40:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+ build (BuildContext context){
+    return MaterialApp(
+      home: Center(child: Text('  Hello world'),),
+    );
+ }
+
+}
